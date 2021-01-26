dc5f1520ceb0c95e4564c3ab7cea029b0512f864
xCONFLiCTiONx <mikehall.outbox@gmail.com>
2021-01-25 18:19:09 -0600

    update

--- Maintenance/Program.cs
+++ Maintenance/Program.cs
@@ -66,7 +66,7 @@ namespace Maintenance
                 settings.ShowDialog();
             }
 
-            EasyLogger.Info("*********************  END OF OPIMIZATIONS  *********************");
+            EasyLogger.Info("*********************  END OF OPTIMIZATIONS  *********************");
 
             Environment.Exit(0);
         }
